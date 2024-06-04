c1a77ada67148bde7fe33ecb2cac484d69ec75c5
Imad Asserne <imad.asserne@gmail.com>
2024-06-04 12:50:36 +0000

    first commit

---
+++ src/main/java/com/IMADWRGH/Test/Controller/Test.java
@@ -0,0 +1,15 @@
+package com.IMADWRGH.Test.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1")
+public class Test {
+
+    @GetMapping(path = "/test")
+    public String test(){
+        return "IMAD";
+    }
+}
